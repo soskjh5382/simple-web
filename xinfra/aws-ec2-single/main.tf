264bc0d836982e9de98facd68465a1c81249cd1d
kjh <soskjh5382@naver.com>
2025-06-21 11:33:44 +0900

    init

--- xinfra/aws-ec2-single/main.tf
+++ xinfra/aws-ec2-single/main.tf
@@ -1,10 +1,75 @@
 provider "aws" {
   region = "ap-northeast-2" # 사용할 AWS 리전
 }
+# VPC 생성
+resource "aws_vpc" "dangtong-vpc" {
+  cidr_block           = "10.0.0.0/16" 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
+  tags = {
+    Name = "dangtong-vpc"
+  }
+}
+# 퍼블릭 서브넷 생성
+resource "aws_subnet" "dangtong-vpc-public-subnet" {
+  for_each = {
+    a = { cidr = "10.0.1.0/24", az = "ap-northeast-2a" }
+    b = { cidr = "10.0.2.0/24", az = "ap-northeast-2b" }
+    c = { cidr = "10.0.3.0/24", az = "ap-northeast-2c" }
+    d = { cidr = "10.0.4.0/24", az = "ap-northeast-2d" }
+  }
+
+  vpc_id                  = aws_vpc.dangtong-vpc.id
+  cidr_block              = each.value.cidr
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dangtong-vpc-public-subnet-${each.key}"
+  }
+}
+
+# 인터넷 게이트웨이 생성
+resource "aws_internet_gateway" "dangtong-igw" {
+  vpc_id = aws_vpc.dangtong-vpc.id
+
+  tags = {
+    Name = "dangtong-igw"
+  }
+}
+
+# 라우팅 테이블 생성
+resource "aws_route_table" "dangtong-vpc-public-rt" {
+  vpc_id = aws_vpc.dangtong-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dangtong-igw.id
+  }
+
+  tags = {
+    Name = "dangtong-vpc-public-rt"
+    
+  }
+}
+
+resource "aws_route_table_association" "dangtong-vpc-public-rt" {
+  for_each = {
+    a = aws_subnet.dangtong-vpc-public-subnet["a"].id
+    b = aws_subnet.dangtong-vpc-public-subnet["b"].id
+    c = aws_subnet.dangtong-vpc-public-subnet["c"].id
+    d = aws_subnet.dangtong-vpc-public-subnet["d"].id
+  }
+  
+  subnet_id      = each.value
+  route_table_id = aws_route_table.dangtong-vpc-public-rt.id
+}
 # 보안 그룹 설정: SSH(22) 및 HTTP(80) 트래픽 허용
 resource "aws_security_group" "nginx_sg" {
   name_prefix = "nginx-sg"
+  vpc_id      = aws_vpc.dangtong-vpc.id
+  
 
   ingress {
     description = "Allow SSH"
@@ -47,7 +112,8 @@ resource "aws_key_pair" "ec2_key" {
    ami             = "ami-08b09b6acd8d62254" # Amazon Linux 2 AMI (리전별로 AMI ID가 다를 수 있음)
    instance_type   = "t2.micro"
    key_name        = aws_key_pair.ec2_key.key_name # AWS에서 생성한 SSH 키 적용
-   security_groups = [aws_security_group.nginx_sg.name]
+   security_groups = [aws_security_group.nginx_sg.id]
+   subnet_id = aws_subnet.dangtong-vpc-public-subnet["a"].id
 
    # EC2 시작 시 Nginx 설치 및 실행을 위한 User Data
    user_data = <<-EOF
